7f860a19aea663f566f06f9f72f375672318ffb9
frankvanrest <frank@dedact.nl>
2015-10-11 18:13:04 +0200

    proof of concept with working example

---
+++ example.js
@@ -0,0 +1,83 @@
+var TVJS = require('./tvjs-tvml-backbone');
+var _ = require('underscore');
+
+
+
+// create router bindings
+var Router = TVJS.Router.extend({
+  routes: {
+    "start":    "start",
+    "second":   "second"
+  },
+
+  start: function(query, page) {
+    console.log("jo");
+    var startView = new StartView();
+    startView.show();
+  },
+
+  second: function(query, page) {
+    var secondView = new SecondView();
+    secondView.show();
+  }
+
+});
+
+console.log(Router);
+
+
+// create a view with events
+var StartView = TVJS.View.extend({
+
+  events: {
+    "select #show-next": "onSelectShowNext"
+  },
+
+  template: _.template("<document>\
+   <alertTemplate>\
+      <title>Welcome</title>\
+      <description>This is the TVJS-TVML-Backbone showcase</description>\
+      <button id='show-next'>\
+         <text>Show next view</text>\
+      </button>\
+      <button>\
+         <text>Nevermind</text>\
+      </button>\
+   </alertTemplate>\
+</document>"),
+
+
+  initialize: function(){
+    this.document = this.parseTemplateString(this.template());
+  },
+
+  onSelectShowNext: function(){
+    myRouter.navigate("second");
+  }
+
+});
+
+var SecondView = TVJS.View.extend({
+  template: _.template("<document>\
+   <ratingTemplate>\
+      <title>Rate TVJS-TVML-Backbone</title>\
+      <ratingBadge value=\"1\"></ratingBadge>\
+   </ratingTemplate>\
+</document>"),
+
+  initialize: function(){
+     this.document = this.parseTemplateString(this.template());
+  }
+
+});
+
+var myRouter = new Router();
+
+App.onLaunch = function(options) {
+  console.log("test");
+  myRouter.navigate("start");
+}
+
+App.onExit = function() {
+    console.log('App finished');
+}
---
+++ tvjs-tvml-backbone.js
@@ -0,0 +1,112 @@
+var Backbone = require('backbone');
+var _ = require('underscore');
+
+// needed by Backbone adjusted methodes
+var pathStripper = /#.*$/;
+var delegateEventSplitter = /^(\S+)\s*(.*)$/;
+
+var TVJSRouter = Backbone.Router.extend({
+
+  navigate: function(fragment, options) {
+
+
+
+      
+      // Normalize the fragment.
+      fragment = Backbone.history.getFragment(fragment || '');
+
+      // Don't include a trailing slash on the root.
+      var root = Backbone.history.root;
+      if (fragment === '' || fragment.charAt(0) === '?') {
+        root = root.slice(0, -1) || '/';
+      }
+      var url = root + fragment;
+
+      // Strip the hash and decode for matching.
+      fragment = Backbone.history.decodeFragment(fragment.replace(pathStripper, ''));
+
+      if (Backbone.history.fragment === fragment) return;
+      Backbone.history.fragment = fragment;
+
+      return this.loadUrl(fragment);
+    },
+
+    // Attempt to load the current URL fragment. If a route succeeds with a
+    // match, returns `true`. If no defined routes matches the fragment,
+    // returns `false`.
+    loadUrl: function(fragment) {
+      // If the root doesn't match, no routes can match either.
+      fragment = Backbone.history.fragment = Backbone.history.getFragment(fragment);
+      return _.some(Backbone.history.handlers, function(handler) {
+        if (handler.route.test(fragment)) {
+          handler.callback(fragment);
+          return true;
+        }
+      });
+    },
+});
+
+var TVJSView = Backbone.View.extend({
+	_ensureElement: function(){
+   	 	return true;
+  	},
+
+  	show: function(){
+		navigationDocument.pushDocument(
+        	this.document
+      	);
+console.log(this.document);
+      	this.delegateEvents();
+	},
+
+	replace: function(){
+		navigationDocument.replaceDocument(
+        	this.document
+		);
+      	this.delegateEvents();
+	},
+
+  	parseTemplateString: function(templateString){
+  		return (new DOMParser() ).parseFromString(
+          templateString,
+          "text/xml"
+        );
+  	},
+
+  	delegateEvents: function(events) {
+      events || (events = _.result(this, 'events'));
+      if (!events) return this;
+      //this.undelegateEvents();
+      for (var key in events) {
+        var method = events[key];
+        if (!_.isFunction(method)) method = this[method];
+        if (!method) continue;
+        var match = key.match(delegateEventSplitter);
+        this.delegate(match[1], match[2], _.bind(method, this));
+      }
+      return this;
+    },
+
+    delegate: function(eventName, selector, listener) {
+
+    	// selector for Id
+    	if(selector.substring(0,1) == "#"){
+    		selector = selector.substring(1);
+    		this.document.getElementById(selector).addEventListener(eventName, listener);		
+
+    	// selector for class
+    	} else if(selector.substring(0,1) == "."){
+    		selector = selector.substring(1);
+    		this.document.getElementByClassName(selector).addEventListener(eventName, listener);
+
+    	// selector for tagName
+    	} else {
+    		this.document.getElementByTagName(selector).addEventListener(eventName, listener);
+    	}
+      return this;
+    },
+
+});
+
+exports.View = TVJSView;
+exports.Router = TVJSRouter;
